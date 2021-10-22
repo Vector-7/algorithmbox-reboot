71c3bfdf7a04c42e4ebeba4824907e73c3757d80
Vector-7 <seokbong60@gmail.com>
2021-10-23 04:50:05 +0900

    update

--- basic/binary_search/binary_search.hs
+++ basic/binary_search/binary_search.hs
@@ -1,23 +1,32 @@
 binarySearch :: (Ord a, Num a) => [a] -> a -> Int
 --  param
---      [Num] => Array List
---      Num => Target Value
+--      [a] => 정렬된 Number 리스트
+--      a => 찾고자 하는 값
 --  return
---      Int => Index
---          if search failed return -1
+--      Int => 해당 값에 해당되는 인덱스
+--          if search failed return -1 실패시 -1 리턴
 binarySearch [] _ = -1      -- 배열이 비어있는 경우 -1 인덱스 반환
 binarySearch arr e = func arr e 0 (length arr - 1)
     where
+        -- func: __binary_search
         func :: (Ord a, Num a) => [a] -> a -> Int -> Int -> Int
+        {-
+            param
+                [a]: 정렬된 Num 리스트
+                a: 찾고자 하는 값
+                Int, Int: left, right
+            return
+                Int: 해당 값에 해당되는 인덱스
+        -}
         func arr e l r
             | l > r             = -1        -- 못찾음
             | target == e       = mid       -- 찾음
             | target > e        = func arr e l (mid - 1)
             | target < e        = func arr e (mid + 1) r
-            | otherwise         = -1
+            | otherwise         = -1        -- else 예외처리
             where
-                mid     = div (l + r) 2
-                target  = arr !! mid
+                mid     = div (l + r) 2     -- 인덱스 중간값
+                target  = arr !! mid        -- 중간 인덱스에 대한 값
 
 
 test :: (Ord a, Num a) => [([a], a)] -> [Int]
