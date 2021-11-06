09421759e1e8bd257516c02ea0e053ba4ac9449b
Vector-7 <seokbong60@gmail.com>
2021-11-06 21:07:46 +0900

    backup

--- basic/graphs/dijkstra/dijkstra.hs
+++ basic/graphs/dijkstra/dijkstra.hs
@@ -1,9 +1,6 @@
 import Data.List
 import qualified Data.Map as M
 
-
 main = do
-    putStrLn (show (test answers testCases))
-    where
-        answers = [0, 3, 6, 99]
-        testCases = []
+    raw <- readFile "case.txt"
+    putStrLn raw
