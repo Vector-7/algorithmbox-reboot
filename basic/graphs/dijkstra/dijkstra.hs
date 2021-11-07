f03e700f035563b9ec8a74cc4b04383c655dc612
Vector-7 <seokbong60@gmail.com>
2021-11-07 19:26:29 +0900

    backup

--- basic/graphs/dijkstra/dijkstra.hs
+++ basic/graphs/dijkstra/dijkstra.hs
@@ -1,5 +1,23 @@
 import Data.List
+import Text.Read
 import qualified Data.Map as M
+
+-- 
+split :: String -> String -> [String]
+-- params: string, delimiter
+-- result: string array
+split "" _ = []
+split str "" = [str]
+split str delimiter = _split str delimiter "" "" []
+    where
+    _split :: String -> String -> String -> String -> [String] -> [String]
+    -- param: string, delimiter, strBuf, bufDel, collectedStr
+    -- return: split list
+    _split curStr delimiter strBuf bufDel collectedStr = collectedStr
+        where
+        curStrFst:curStrOther = curStr
+
+
 
 main = do
     raw <- readFile "case.txt"
