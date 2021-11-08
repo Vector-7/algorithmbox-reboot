15c84e1829f68fc262a016de7a2eb7a02e577401
Vector-7 <seokbong60@gmail.com>
2021-11-08 16:59:46 +0900

    다익스트라 무한루프 문제 해결 필요

--- basic/graphs/dijkstra/dijkstra.hs
+++ basic/graphs/dijkstra/dijkstra.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Text.Read
 import qualified Data.Map as M
+import Debug.Trace
 
 data TestCase = 
     TestCase Int Int [[Int]] [Int]
@@ -57,31 +58,82 @@ parse str = _parse (split '\n' str) 0
                 starts  = split ' ' (splited !! startLoc)
                 ends    = split ' ' (splited !! (startLoc + 1))
                 weights = split ' ' (splited !! (startLoc + 2))
-                _makeEdgeList (s:sx) (e:ex) (w:wx)
-                    | starts == []      = []
-                    | otherwise         = [(read s :: Int), (read e :: Int), (read w :: Int)]:(_makeEdgeList sx ex wx)
+                _makeEdgeList :: [String] -> [String] -> [String] -> [[Int]]
+                _makeEdgeList [] [] [] = []
+                _makeEdgeList (s:sx) (e:ex) (w:wx) = [(read s :: Int), (read e :: Int), (read w :: Int)]:(_makeEdgeList sx ex wx)
             makeExpectedList :: [String] -> [Int]
             {-- 예상 정답 리스트 생성 --}
             makeExpectedList = map (\e -> read e :: Int)
 
-test :: [TestCase] -> [(String, [[Int]])]
+
+covertResultMap2Array :: (Num a, Ord a) => M.Map a a -> a -> a -> [a]
+covertResultMap2Array resultMap start end
+    | start == end  = [resultMap M.! start]
+    | otherwise     = (resultMap M.! start):(covertResultMap2Array resultMap (start + 1) end)
+
+test :: [TestCase] -> [([Int], [Int])]
 --  param: TestCase List
 --  return: [(Success or Failed, expectedList, actualList)]
 test [] = []
-test (t:ts) = ((checkIsSuccess expected acutal), [expected, acutal]) : (test ts)
+test (t:ts) = (expected, acutal) : (test ts)
     where
     vertexNum   = getVertexNums t
     edgeNum     = getEdgeNums t
     edges       = getEdges t
     expected    = getExpectedResult t
-    acutal      = dijkstra vertexNum edgeNum edges
-    checkIsSuccess :: [Int] -> [Int] -> String
-    checkIsSuccess [] [] = "SUCCESS"
-    checkIsSuccess (e:es) (a:as)
-        | e /= a        = "FAILED"
-        | otherwise     = checkIsSuccess es as
+    acutal      = covertResultMap2Array (dijkstra vertexNum edgeNum edges) 1 vertexNum
+
+{-- MAIN ALGORITHM --}
+makeGraph :: (Num a, Ord a) => [[a]] -> M.Map a [(a, a)]
+makeGraph [] = M.fromList []
+makeGraph (x:xs)
+    where
+    g = makeGraph xs
+
+dijkstra :: (Num a, Ord a) => a -> a -> [[a]] -> M.Map a a
+-- param: vertexNum, edgeNum, edges 
+-- return: weight result list
+dijkstra vertexNum edgeNum edges = _dijkstra graph wList queue
+    where
+    queue = [(1, 0)]
+    wList = _initWeightList 1 vertexNum
+    graph = makeGraph edges
+    _initWeightList :: (Num a, Ord a) => a -> a -> M.Map a a
+    -- 가중치 리스트 초기화
+    _initWeightList start end
+        | start == end      = M.fromList[(end, -1)]
+        | otherwise         = M.insert start (-1) (_initWeightList (start + 1) end)
+    _dijkstra :: (Ord a, Num a) => M.Map a [(a, a)] -> M.Map a a -> [(a, a)] -> M.Map a a
+    -- 다익스트라 루틴 함수 (큐가 다 빌 때 가지 진행)
+    _dijkstra graph wList [] = wList
+    _dijkstra graph wList (q:qs)
+        | isAleadyExists == False   = _dijkstra graph newWList (updateQ (graph M.! point))
+        | otherwise                 = _dijkstra graph newWList (updateQ [])
+        where
+        point = fst q
+        weight = snd q
+        updatedW        = _updateWList newWList point weight
+        isAleadyExists  = fst updatedW
+        newWList        = snd updatedW
+        updateQ         = _updateQueue qs weight
+        _updateWList :: (Ord a, Num a) => M.Map a a -> a -> a -> (Bool, M.Map a a)
+        -- 가중치 리스트 갱신
+        -- return: (isAleadyExists, Updated W List)
+        _updateWList newWList point weight
+            | prevWeight == -1      = (False, M.adjust (const weight) point newWList)
+            | otherwise             = (True, M.adjust ((const . (max weight)) prevWeight) point newWList)
+            where
+            prevWeight = newWList M.! point
+        _updateQueue :: (Ord a, Num a) => [(a, a)] -> a -> [(a, a)] -> [(a, a)]
+        -- 큐에 새 데이터 추가
+        -- param: Queue, New Element List, +Weight
+        _updateQueue q _ [] = q
+        _updateQueue q w (n:ns) = _updateQueue (q ++ [(fst n, (snd n) + w)]) w ns
 
 main = do
     raw <- readFile "case.txt"
     let testCases = parse raw
-    putStrLn (show (test testCases))
+    let edge0 = getEdges (testCases !! 0)
+    -- putStrLn (show (makeGraph edge0))
+    putStrLn (show (getEdges (testCases !! 0)))
+    -- putStrLn (show (test (parse raw)))
