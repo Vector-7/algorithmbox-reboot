48f111e28fdcc930a630d1f92eaf298339df40f9
Vector-7 <seokbong60@gmail.com>
2021-11-09 21:28:01 +0900

    다익스트라 알고리즘 구현

--- basic/graphs/dijkstra/dijkstra.hs
+++ basic/graphs/dijkstra/dijkstra.hs
@@ -66,7 +66,7 @@ parse str = _parse (split '\n' str) 0
             makeExpectedList = map (\e -> read e :: Int)
 
 
-covertResultMap2Array :: (Num a, Ord a) => M.Map a a -> a -> a -> [a]
+covertResultMap2Array :: (Ord a, Num a, Show a) => M.Map a a -> a -> a -> [a]
 covertResultMap2Array resultMap start end
     | start == end  = [resultMap M.! start]
     | otherwise     = (resultMap M.! start):(covertResultMap2Array resultMap (start + 1) end)
@@ -84,19 +84,19 @@ test (t:ts) = (expected, acutal) : (test ts)
     acutal      = covertResultMap2Array (dijkstra vertexNum edgeNum edges) 1 vertexNum
 
 {-- MAIN ALGORITHM --}
-makeGraph :: (Num a, Ord a) => [[a]] -> M.Map a [(a, a)]
+makeGraph :: (Ord a, Num a, Show a) => [[a]] -> M.Map a [(a, a)]
 makeGraph [] = M.fromList []
 makeGraph (x:xs) = _makeGraph p2 p1 w (_makeGraph p1 p2 w (makeGraph xs))
     where
     p1 = x !! 0
     p2 = x !! 1
     w = x !! 2
-    _makeGraph :: (Num a, Ord a) => a -> a -> a -> M.Map a [(a, a)] -> M.Map a [(a, a)]
+    _makeGraph :: (Ord a, Num a, Show a) => a -> a -> a -> M.Map a [(a, a)] -> M.Map a [(a, a)]
     _makeGraph start end weight g
-        | M.member start g      = M.insert start [(end, weight)] g
-        | otherwise             = M.adjust ((end, weight):) end g
+        | not (M.member start g)    = M.insert start [(end, weight)] g
+        | otherwise                 = M.adjust ((end, weight):) start g
 
-dijkstra :: (Num a, Ord a) => a -> a -> [[a]] -> M.Map a a
+dijkstra :: (Ord a, Num a, Show a) => a -> a -> [[a]] -> M.Map a a
 -- param: vertexNum, edgeNum, edges 
 -- return: weight result list
 dijkstra vertexNum edgeNum edges = _dijkstra graph wList queue
@@ -104,43 +104,35 @@ dijkstra vertexNum edgeNum edges = _dijkstra graph wList queue
     queue = [(1, 0)]
     wList = _initWeightList 1 vertexNum
     graph = makeGraph edges
-    _initWeightList :: (Num a, Ord a) => a -> a -> M.Map a a
+    _initWeightList :: (Ord a, Num a, Show a) => a -> a -> M.Map a a
     -- 가중치 리스트 초기화
     _initWeightList start end
-        | start == end      = M.fromList[(end, -1)]
+        | start >= end      = M.fromList [(end, -1)]
         | otherwise         = M.insert start (-1) (_initWeightList (start + 1) end)
-    _dijkstra :: (Ord a, Num a) => M.Map a [(a, a)] -> M.Map a a -> [(a, a)] -> M.Map a a
+    _dijkstra :: (Ord a, Num a, Show a) => M.Map a [(a, a)] -> M.Map a a -> [(a, a)] -> M.Map a a
     -- 다익스트라 루틴 함수 (큐가 다 빌 때 가지 진행)
     _dijkstra graph wList [] = wList
-    _dijkstra graph wList (q:qs)
-        | isAleadyExists == False   = _dijkstra graph newWList (updateQ (graph M.! point))
-        | otherwise                 = _dijkstra graph newWList (updateQ [])
+    _dijkstra graph wList (e:q)
+        | isAleadyVisited == True   = _dijkstra graph newWList (updateQ [])
+        | otherwise                 = _dijkstra graph newWList (updateQ (graph M.! point))
         where
-        point = fst q
-        weight = snd q
-        updatedW        = _updateWList newWList point weight
-        isAleadyExists  = fst updatedW
-        newWList        = snd updatedW
-        updateQ         = _updateQueue qs weight
-        _updateWList :: (Ord a, Num a) => M.Map a a -> a -> a -> (Bool, M.Map a a)
-        -- 가중치 리스트 갱신
-        -- return: (isAleadyExists, Updated W List)
-        _updateWList newWList point weight
-            | prevWeight == -1      = (False, M.adjust (const weight) point newWList)
-            | otherwise             = (True, M.adjust ((const . (max weight)) prevWeight) point newWList)
+        point       = fst e
+        weight      = snd e
+        updatedListNVisited = _updateList wList point weight
+        isAleadyVisited     = fst updatedListNVisited
+        newWList            = snd updatedListNVisited
+        updateQ             = _updateQueue q weight
+        _updateList :: (Ord a, Num a, Show a) => M.Map a a -> a -> a -> (Bool, M.Map a a)
+        _updateList list point weight
+            | prevWeight == -1  = (False, M.adjust (const weight) point list)
+            | otherwise         = (True, M.adjust ((const . (min weight)) prevWeight) point list)
             where
-            prevWeight = newWList M.! point
-        _updateQueue :: (Ord a, Num a) => [(a, a)] -> a -> [(a, a)] -> [(a, a)]
-        -- 큐에 새 데이터 추가
-        -- param: Queue, New Element List, +Weight
+            prevWeight = list M.! point
+        _updateQueue :: (Ord a, Num a, Show a) => [(a, a)] -> a -> [(a, a)] -> [(a, a)]
         _updateQueue q _ [] = q
         _updateQueue q w (n:ns) = _updateQueue (q ++ [(fst n, (snd n) + w)]) w ns
 
 main = do
     raw <- readFile "case.txt"
     let testCases = parse raw
-    let edge0 = getEdges (testCases !! 0)
-    putStrLn (show edge0)
-    let g = makeGraph edge0
-    putStrLn (show g)
-    -- putStrLn (show (test (parse raw)))
+    putStrLn (show (test testCases))
