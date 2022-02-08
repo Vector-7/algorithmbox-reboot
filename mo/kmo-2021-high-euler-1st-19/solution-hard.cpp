5f0dffaec15691e1c3d20ac4ec5b44c6a23a1d75
Vector-7 <seokbong60@gmail.com>
2022-02-09 04:26:44 +0900

    물웅덩이

--- mo/kmo-2021-high-euler-1st-19/solution-hard.cpp
+++ mo/kmo-2021-high-euler-1st-19/solution-hard.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <vector>
+#define MODULA 1000000007
 
 using namespace std;
 typedef long long ll;
+
+ll ans = 0;
+int R, C, N;
+vector<ll> facts;
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    
+    cin >> R >> C;
+    cin >> N;
 
+    facts = (R > C) ? vector<ll>(R) : vector<ll>(C);
+    facts[0] = 1;
+    
+    // 팩토리얼
+    for(int i = 1; i < (int)facts.size(); i++)
+        facts[i]  = (facts[i] * facts[i-1]) % MODULA;
 
     return 0;
 }
